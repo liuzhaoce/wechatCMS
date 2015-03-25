5446a5a8f02e4a84d2d317fd57cdc443493cb70e
xiaobaicaistring <>
2015-03-25 23:27:41 +0800

    no message

--- wechat.js
+++ wechat.js
@@ -46,7 +46,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
